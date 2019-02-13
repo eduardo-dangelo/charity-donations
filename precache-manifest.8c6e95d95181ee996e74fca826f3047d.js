a1d4f2fbf7de3512371ed6230007558cfceaa1c6
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2019-02-13 21:55:36 +0000

    Updates

---
+++ precache-manifest.8c6e95d95181ee996e74fca826f3047d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d817d96efb64ac19a85",
+    "url": "/charity-donations/static/js/runtime~main.4d817d96.js"
+  },
+  {
+    "revision": "cedfffc0692ce81ec20e",
+    "url": "/charity-donations/static/js/main.cedfffc0.chunk.js"
+  },
+  {
+    "revision": "7081a2a82ad0ed48239d",
+    "url": "/charity-donations/static/js/2.7081a2a8.chunk.js"
+  },
+  {
+    "revision": "cedfffc0692ce81ec20e",
+    "url": "/charity-donations/static/css/main.0375435e.chunk.css"
+  },
+  {
+    "revision": "5c7232a1c5c4639d73dba85f244cfb0d",
+    "url": "/charity-donations/index.html"
+  }
+];
